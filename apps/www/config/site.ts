cbeee1653ba4607f34cf4183a117f7962411e355
Vladimir Semenov <vovsemenv@gmail.com>
2023-05-08 23:35:43 +0300

    some fixes

--- apps/www/config/site.ts
+++ apps/www/config/site.ts
@@ -1,7 +1,12 @@
 export const siteConfig = {
   name: "uo-patterns-diploma",
   url: "https://ui-patters-diploma.vercel.app",
-  description: "Vladimir Semyonov UI patterns diploma",
+  ogImage: "https://invalid-urls-just-for-tests.ru",
+  description: "Vladimir Semyonov ui patterns diploma",
+  links: {
+    twitter: "https://invalid-urls-just-for-tests.ru",
+    github: "https://invalid-urls-just-for-tests.ru",
+  },
 }
 
 export type SiteConfig = typeof siteConfig
